f66aeee802c5d43ffa2d69e149869dc33bd5b39f
styletab <katherinetab@gmail.com>
2016-09-19 13:51:11 -0400

    Update Initialize State
    - Added in initialize.state.each
    - defined come_alive on cell class

--- lib/conway.rb
+++ lib/conway.rb
@@ -1,7 +1,4 @@
-ok what should we shell out first?
-"shell out?"
-
-liek what class do we wantt o define
+require 'pry'
 
 # start here?
   class World
@@ -10,9 +7,22 @@ liek what class do we wantt o define
     def initialize(size, initial_state)
       @cells = Array.new(size**2)
       @cells.each_index {|i| @cells[i] = Cell.new}
+      # write code to set cells marked in initial_state to alive = true
+      initial_state.each {|i| @cells[i].come_alive}
     end
   end
 
 
   class Cell
+    def initialize
+      @alive = false
+    end
+
+    def come_alive
+      @alive = true
+    end
   end
+
+  binding.pry
+''
+commit
